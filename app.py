7feec9f16b47b8de4d29ba70459fc5ab9d19e789
Harishhrds <140880280+Harishhrds@users.noreply.github.com>
2025-07-30 16:28:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ with open(model_path,'rb') as file:
 app= Flask(__name__)
 app.secret_key = "SanathWonder2466"
 CORS(app) # 
-loader = PyPDFLoader("Asset Chatbot.pdf")
+loader = PyPDFLoader("https://github.com/Harishhrds/Asset_lifecycle_prediciton/blob/main/Asset%20Chatbot.pdf")
 docs = loader.load()
 
 
