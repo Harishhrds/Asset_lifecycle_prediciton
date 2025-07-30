76adb86d1dc11c7b5e7722eb8b518f67481dd191
Harishhrds <140880280+Harishhrds@users.noreply.github.com>
2025-07-30 15:37:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask,render_template,jsonify,request,redirect,url_for,session
 import pickle
 import pandas as pd
 import numpy as np
-from flask_mysqldb import MySQL
 from dotenv import load_dotenv
+import psycopg2
 import os
+load_dotenv()
 #langchain-setup
 from flask_cors import CORS
 from langchain_community.document_loaders import PyPDFLoader
@@ -22,7 +23,7 @@ with open(model_path,'rb') as file:
 app= Flask(__name__)
 app.secret_key = "SanathWonder2466"
 CORS(app) # 
-loader = PyPDFLoader("Asset Chatbot.pdf")
+loader = PyPDFLoader(r"C:\Users\Lenovo\Desktop\Asset_Life_cycle_web\Asset Chatbot.pdf")
 docs = loader.load()
 
 
@@ -47,13 +48,23 @@ def chat():
     result=qa.run(user_msg)
     return jsonify({"reply":result})
 
-# MySQL configuration
-app.config['MYSQL_HOST'] = os.getenv('DB_HOST')
-app.config['MYSQL_USER'] = os.getenv('DB_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('DB_NAME')
+model_path = "asset_life.pkl"
+with open(model_path,'rb') as file:
+    model = pickle.load(file)
 
-mysql = MySQL(app)
+app= Flask(__name__, template_folder='templates')
+app.secret_key = "SanathWonder2466"
+
+# postgress SQL 
+
+conn = psycopg2.connect(
+    host=os.getenv('PGHOST'),
+    port=os.getenv('PGPORT'),
+    user=os.getenv('PGUSER'),
+    password=os.getenv('PGPASSWORD'),
+    dbname=os.getenv('PGDATABASE')
+)
+
 @app.route('/signup',methods=['GET','POST'])
 def signup():
     if request.method == 'POST':
@@ -61,7 +72,7 @@ def signup():
         phone = request.form.get('phone_number')
         username = request.form.get('username')
         password = request.form.get('password')
-        cur = mysql.connection.cursor()
+        cur = conn.cursor()
         cur.execute("SELECT * FROM users WHERE username=%s",(username,))
         user = cur.fetchone()
         if user:
@@ -69,7 +80,7 @@ def signup():
             return render_template("signup.html")  
         cur.execute("INSERT INTO users(full_name,phone,username,password) VALUES(%s,%s,%s,%s)",
                     (full_name,phone,username,password))
-        mysql.connection.commit()
+        conn.commit()
         cur.close()
         flash("Signup Successful.Please login")
         return redirect(url_for('login'))
@@ -79,7 +90,7 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        cur = mysql.connection.cursor()
+        cur = conn.cursor()
         cur.execute("SELECT * FROM users WHERE username=%s AND password=%s", (username, password))
 
         user = cur.fetchone()
